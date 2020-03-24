118b7dd35ca52b7bf9a8fc42dd0fdb8df5736049
David Bullado <david.bullado@gmail.com>
2020-03-24 19:54:35 +0100

    Create setup-cifs-automount.sh

---
+++ setup-cifs-automount.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo tee /etc/systemd/system/mnt-downloads.mount > /dev/null <<EOT
+[Unit]
+Description=Freebox download folder
+After=network-online.target
+Wants=network-online.target
+
+[Mount]
+What=//192.168.1.254/disk/Téléchargements
+Where=/mnt/downloads
+Type=cifs
+Options=username=freebox,password=mypassword,iocharset=utf8,file_mode=0777,dir_mode=0777,vers=1.0
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+
+sudo tee /etc/systemd/system/mnt-downloads.automount > /dev/null <<EOT
+[Unit]
+Description=Auto mount Freebox download folder
+After=network-online.target
+Wants=network-online.target
+
+[Automount]
+Where=/mnt/downloads
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable mnt-downloads.automount
+sudo systemctl start mnt-downloads.automount
+ls /mnt/downloads
